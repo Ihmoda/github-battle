a81717b58e09be569f850ed095cdf2027ae14baf
Omar Ihmoda <omarihmoda@DN0a226517.SUNet>
2018-02-05 09:57:31 -0800

    initial commit

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,19 +3,12 @@ const PropTypes = require('prop-types');
 const api = require('../utils/api');
 
 function SelectLanguage(props) {
-  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
-
+  const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python', 'C#', 'Swift', 'Rust', 'Go', 'Haskell', 'R'];
   return (
     <ul className="languages">
       {languages.map(lang => (
         <li
-          style={
-            lang === props.selectedLanguage
-              ? {
-                color: '#d0021b:'
-              }
-              : null
-          }
+          style={lang === props.selectedLanguage ? {color: '#d0021b'} : null}
           onClick={props.onSelect.bind(null, lang)}
           key={lang}
         >
@@ -26,18 +19,31 @@ function SelectLanguage(props) {
   );
 }
 
-// function RepoGrid(props) {
-//   return (
-//     <ul className="popular-list">
-//       {props.repos.map((repo, index) => {
-//         <li key={repo.name} className={repo.list}>
-//           <div className="popular-rank">#{index + 1}</div>
-//           <ul className="space-list-item">Hello</ul>
-//         </li>;
-//       })}
-//     </ul>
-//   );
-// }
+function RepoGrid(props) {
+  return (
+    <ul className="popular-list">
+      {props.repos.map((repo, index) => (
+        <li key={repo.name} className="popular-item">
+          <div className="popular-rank">#{index + 1}</div>
+          <ul className="space-list-items">
+            <li>
+              <img className="avatar" src={repo.owner.avatar_url} alt={`Avatar for ${repo.owner.login}`} />
+            </li>
+            <li>
+              <a href={repo.html_url}>{repo.name}</a>
+            </li>
+            <li>@{repo.owner.login}</li>
+            <li>{repo.stargazers_count} stars</li>
+          </ul>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired
+};
 
 SelectLanguage.propTypes = {
   selectedLanguage: PropTypes.string.isRequired,
@@ -46,30 +52,34 @@ SelectLanguage.propTypes = {
 
 class Popular extends React.Component {
   constructor(props) {
-    super(props);
+    super();
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null
     };
 
     this.updateLanguage = this.updateLanguage.bind(this);
   }
-
   componentDidMount() {
     this.updateLanguage(this.state.selectedLanguage);
   }
-
   updateLanguage(lang) {
-    this.setState(() => ({selectedLanguage: lang, repos: null}));
+    this.setState(() => ({
+      selectedLanguage: lang,
+      repos: null
+    }));
 
     api.fetchPopularRepos(lang).then(repos => {
-      this.setState(() => ({repos: repos}));
+      this.setState(() => ({
+        repos: repos
+      }));
     });
   }
-
   render() {
     return (
       <div>
         <SelectLanguage selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage} />
+        {!this.state.repos ? <p>LOADING!</p> : <RepoGrid repos={this.state.repos} />}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,21 @@ var path = require('path');
 var HhtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: "./app/index.js",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'index_bundle.js'
-    },
-    module: {
-        rules: [
-            { test: /\.(js)$/, use: 'babel-loader' },
-            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] }
-        ]
-    },
-    plugins: [new HhtmlWebpackPlugin({
-        template: 'app/index.html'
-    })]
-}
+  entry: './app/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index_bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [{ test: /\.(js)$/, use: 'babel-loader' }, { test: /\.css$/, use: ['style-loader', 'css-loader'] }],
+  },
+  devServer: {
+    historyApiFallback: true,
+  },
+  plugins: [
+    new HhtmlWebpackPlugin({
+      template: 'app/index.html',
+    }),
+  ],
+};
